f592281de5162a00b0c9efd1cf1c88802765539c
Flux <flustixshinu@gmail.com>
2022-06-05 21:10:35 +0200

    background parallax

---
+++ assets/script/parallax.js
@@ -0,0 +1,11 @@
+document.addEventListener("mousemove", parallax);
+
+function parallax(m) {
+    var windowW = window.innerWidth / 2;
+    var windowH = window.innerHeight / 2;
+
+    var mouseX = m.clientX;
+    var mouseY = m.clientY;
+
+    document.body.style.backgroundPosition = `${50 - (mouseX - windowW) * 0.01}% ${50 - (mouseY - windowH) * 0.01}%`;
+}
